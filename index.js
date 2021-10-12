6a31dc3c1355589ae5f96888d601c0f58656250f
Jahi Stewart <jahikstewart@lewisu.edu>
2021-10-12 05:23:02 -0500

    Bug fix

--- index.js
+++ index.js
@@ -2,15 +2,33 @@ const http = require('http')
 const fs = require('fs')
 const port = process.env.PORT || 3000
 
-const server = http.createServer((req,res) => {
-    // normalize url by removing querystring, optional trailing slash, and
-    // making lowercase
-    const path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase()
-    switch(path) {
-      case '':
-        serveStaticFile(res, '/public/home.html', 'text/html')
-        break
+function serveStaticFile(res, path, contentType, responseCode = 200) {
+  fs.readFile(__dirname + path, (err, data) => {
+    if(err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' })
+      return res.end('500 - Internal Error')
     }
+    res.writeHead(responseCode, { 'Content-Type': contentType })
+    res.end(data)
   })
+}
+
+const server = http.createServer((req,res) => {
+  // normalize url by removing querystring, optional trailing slash, and
+  // making lowercase
+  const path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase()
+  switch(path) {
+    case '':
+      serveStaticFile(res, '/home.html', 'text/html')
+      break
+    case '/backyard.png':
+        serveStaticFile(res, '/backyard.png', 'image/png')
+        break
+    default:
+      serveStaticFile(res, '/404.html', 'text/html', 404)
+      break
+  }
+})
+
 server.listen(port, () => console.log(`server started on port ${port}; ` +
-'press Ctrl-C to terminate....'))
+  'press Ctrl-C to terminate....'))
